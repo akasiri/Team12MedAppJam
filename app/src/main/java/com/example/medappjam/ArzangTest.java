385a417c07d27f824955850994ad46e959e4a444
akasiri <akasiri@uci.edu>
2016-11-17 22:52:53 -0800

    Sign Up intermediate commit

--- app/src/main/java/com/example/medappjam/ArzangTest.java
+++ app/src/main/java/com/example/medappjam/ArzangTest.java
@@ -2,6 +2,7 @@ package com.example.medappjam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ArzangTest extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class ArzangTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arzang_test);
+    }
+
+    public void submitSignUp(View view) {
+
+
+
+        super.finish();
+        finish();
     }
 }
