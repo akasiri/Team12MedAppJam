b3637f41714ce2359869e79f9844d0de0e97dd81
hellojjangyool <yulimc@uci.edu>
2016-11-21 00:31:34 -0800

    ToolbarAdded.StillneedaFix

--- app/src/main/java/com/example/medappjam/About.java
+++ app/src/main/java/com/example/medappjam/About.java
@@ -4,5 +4,12 @@ package com.example.medappjam;
  * Created by User on 11/20/2016.
  */
 
-public class About {
+import android.os.Bundle;
+
+public class About extends AppCompatPreferenceActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
 }
