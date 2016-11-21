16bd150e9178c3d535996ccb1e0b58ba1576b4ac
akasiri <akasiri@uci.edu>
2016-11-21 15:10:41 -0800

    setup programatic generation of provider list

--- app/src/main/java/com/example/medappjam/MyProvidersActivity.java
+++ app/src/main/java/com/example/medappjam/MyProvidersActivity.java
@@ -1,9 +1,13 @@
 package com.example.medappjam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MyProvidersActivity extends AppCompatActivity {
     private Button mAddProvider;
@@ -13,12 +17,36 @@ public class MyProvidersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_providers);
 
-        mAddProvider = (Button) findViewById(R.id.add_provider);
-        mAddProvider.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //add provider
-            }
-        });
+        // auto generate providers
+        LinearLayout container =(LinearLayout) findViewById(R.id.providers_container);
+        for(int i = 0; i < 5; i++) {
+            TextView providerName = new TextView(this);
+            // TODO get provider name from database
+            providerName.setText("Provider "+i);
+
+            LinearLayout phonenumberContainer = new LinearLayout(this);
+            phonenumberContainer.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView phonenumPrompt = new TextView(this);
+            phonenumPrompt.setText("Phone Number: ");
+            TextView phonenumber = new TextView(this);
+            // TODO get provider number from database
+            phonenumber.setText("");
+
+            phonenumberContainer.addView(phonenumPrompt);
+            phonenumberContainer.addView(phonenumber);
+
+            TextView blank = new TextView(this);
+
+            container.addView(providerName);
+            container.addView(phonenumberContainer);
+            container.addView(blank);
+
+        }
+    }
+
+    public void addProvider(View view) {
+        Intent intent = new Intent(this, AddProviderActivity.class);
+        startActivity(intent);
     }
 }
